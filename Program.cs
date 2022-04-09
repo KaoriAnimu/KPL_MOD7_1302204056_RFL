d06674742dcdc9260d28ca77bed73aa7c84882dc
KaoriAnimu <mkaorirahadj89@gmail.com>
2022-04-09 17:53:07 +0700

    File Jurnal MOD 7

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KPL_MOD7
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try {
+
+                BankTransferConfig runtime = new BankTransferConfig();
+
+                runtime.konfigurasi();
+
+            } catch (Exception ex) {
+
+                Console.WriteLine(ex.ToString());
+
+            }
+        }
+    }
+}
